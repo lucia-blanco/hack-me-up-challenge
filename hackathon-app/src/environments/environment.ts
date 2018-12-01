bdfdfda67978ec36f34ba1d4fdc605bba69edba4
Lucía Blanco <lublasal@gmail.com>
2018-12-01 16:05:05 +0100

    ts lint

--- hackathon-app/src/environments/environment.ts
+++ hackathon-app/src/environments/environment.ts
@@ -7,13 +7,13 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCHe_2P5iWV28fxqJ0BfH91DxTbGoGFHS4",
-  authDomain: "challenge-aad5b.firebaseapp.com",
-  databaseURL: "https://challenge-aad5b.firebaseio.com",
-  projectId: "challenge-aad5b",
-  storageBucket: "challenge-aad5b.appspot.com",
-  messagingSenderId: "736651861630"
-}; 
+  apiKey: 'AIzaSyCHe_2P5iWV28fxqJ0BfH91DxTbGoGFHS4',
+  authDomain: 'challenge-aad5b.firebaseapp.com',
+  databaseURL: 'https://challenge-aad5b.firebaseio.com',
+  projectId: 'challenge-aad5b',
+  storageBucket: 'challenge-aad5b.appspot.com',
+  messagingSenderId: '736651861630'
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
