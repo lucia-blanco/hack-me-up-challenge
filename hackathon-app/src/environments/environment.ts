f325a57dc1354f73cd38974e078f61d30d46c3fb
Abel Mescua <abelmescua@gmail.com>
2018-12-01 15:59:33 +0100

    Firebase added

--- hackathon-app/src/environments/environment.ts
+++ hackathon-app/src/environments/environment.ts
@@ -6,6 +6,15 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCHe_2P5iWV28fxqJ0BfH91DxTbGoGFHS4",
+  authDomain: "challenge-aad5b.firebaseapp.com",
+  databaseURL: "https://challenge-aad5b.firebaseio.com",
+  projectId: "challenge-aad5b",
+  storageBucket: "challenge-aad5b.appspot.com",
+  messagingSenderId: "736651861630"
+}; 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
