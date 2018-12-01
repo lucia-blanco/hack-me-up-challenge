663b6c23f7557298097c808b6456b660b9b6dae4
Lucía Blanco <lublasal@gmail.com>
2018-12-01 15:57:43 +0100

    components

--- hackathon-app/src/app/app.module.ts
+++ hackathon-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RankingComponent } from './componentes/ranking/ranking.component';
+import { HackathonComponent } from './componentes/hackathon/hackathon.component';
+import { ParticipantesComponent } from './componentes/participantes/participantes.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { LandingComponent } from './componentes/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RankingComponent,
+    HackathonComponent,
+    ParticipantesComponent,
+    DashboardComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
